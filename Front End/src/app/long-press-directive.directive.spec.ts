65bc998f77ea04722178dc4d068506ea3336c15a
Chetan Alla <allachetan@Chetans-MBP.fios-router.home>
2020-08-17 16:20:05 -0400

    commiting all files to host frontend

--- Front End/src/app/long-press-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LongPressDirectiveDirective } from './long-press-directive.directive';
-
-describe('LongPressDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LongPressDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Front End/src/app/short-press-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShortPressDirectiveDirective } from './short-press-directive.directive';
-
-describe('ShortPressDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShortPressDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
